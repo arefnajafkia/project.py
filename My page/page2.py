ce7e88ae77214e7dd3ba74ea261e566fb60a193c
aref najafkia <arefnajafkia294@gmail.com>
2022-05-24 20:22:11 +0430

    Added new file My Car5,6 ans Mt page4,5

---
+++ My page/page2.py
@@ -0,0 +1,35 @@
+
+# حدس زدن شماره انتخابی توسط کامپیوتر
+
+
+from math import floor
+
+def IsGuessTrue(Min, Max, Guess, NoGuess):
+    if Min == Max :
+        return
+    else:
+        OP = input ("Is your number (E)qual to ,(G)reater than or (L)ess than %i: " % Guess )
+        if (OP == 'E' or OP == 'e'):
+            print ("I found your number in %i Guess , it is %i"% (NoGuess, Guess))
+            Max = Min
+            IsGuessTrue(Min, Max, Guess, NoGuess)
+        elif (OP == 'G' or OP == 'g'):
+            Min = Guess
+            Guess = floor ((Min + Max)/2)
+            NoGuess += 1
+            IsGuessTrue(Min, Max, Guess, NoGuess)
+        elif (OP == 'L' or OP == 'l'):
+            Max = Guess
+            Guess = floor ((Min + Max)/2)
+            NoGuess += 1
+            IsGuessTrue(Min, Max, Guess, NoGuess)
+        else: 
+             print ("Error in data")
+             
+
+Numbers = [x for x in range (1001)]
+Min = 1
+Max = 1000
+Guess = floor ((Min + Max)/2)
+NoGuess = 1
+IsGuessTrue(Min, Max, Guess, NoGuess)
