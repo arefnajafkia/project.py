d87670d712f336e44619610b2c1ee1324b523301
aref najafkia <arefnajafkia294@gmail.com>
2022-06-03 22:04:12 +0430

    added new modi my page9.py تنظیم درجه حرارت آیگرمکن

---
+++ My page/page9.py
@@ -0,0 +1,135 @@
+
+# برنامه نوشته شده برای تنظیم درجه حرارت آبگرمکن
+
+Degree = int(input("  درجه رالطفا تنظیم کنید ؟\nPlease adjust the degree :"))
+if  Degree <= 50:
+    print(" درجه کمه بیشترش کن :\nThe degree decreased. Do more:")
+elif Degree  <=59:
+    print(" درجه رایه کم زیادکن :\nThe degree decreased. Do more :")
+elif Degree == 60:
+    print("بهترین تنظیم کارخانه :\nBest factory setting:")
+elif Degree  <= 69:
+    print(" درجه رایه کم کمش کن :\nIncrease the degree :")
+elif Degree  <= 79:
+    print(" درجه زیادشدکمش کن :\nIncrease the degree :")
+elif Degree <= 99:
+    print(" درجه خیلی زیاد شدکمش کن :\nIncrease the degree :")
+
+    print("لطفا توجه کنید !\nplease pay attention!")
+    # تکرارشد
+Degree = int(input("  درجه را تنظیم کنید ؟\nPlease adjust the degree :"))
+if  Degree <= 50:
+    print(" درجه کمه بیشترش کن :\nThe degree decreased. Do more:")
+elif Degree  <=59:
+    print(" درجه رایه کم زیادکن :\nThe degree decreased. Do more :")
+elif Degree == 60:
+    print("بهترین تنظیم کارخانه :\nBest factory setting:")
+elif Degree  <= 69:
+    print(" درجه رایه کم کمش کن :\nIncrease the degree :")
+elif Degree  <= 79:
+    print(" درجه زیادشدکمش کن :\nIncrease the degree :")
+elif Degree <= 99:
+    print(" درجه خیلی زیاد شدکمش کن :\nIncrease the degree :")
+
+    print("لطفا توجه کنید !\nplease pay attention!")
+    # تکرارشد
+Degree = int(input("  درجه را تنظیم کنید ؟\nPlease adjust the degree :"))
+if  Degree <= 50:
+    print(" درجه کمه بیشترش کن :\nThe degree decreased. Do more:")
+elif Degree  <=59:
+    print(" درجه رایه کم زیادکن :\nThe degree decreased. Do more :")
+elif Degree == 60:
+    print("بهترین تنظیم کارخانه :\nBest factory setting:")
+elif Degree  <= 69:
+    print(" درجه رایه کم کمش کن :\nIncrease the degree :")
+elif Degree  <= 79:
+    print(" درجه زیادشدکمش کن :\nIncrease the degree :")
+elif Degree <= 99:
+    print(" درجه خیلی زیاد شدکمش کن :\nIncrease the degree :")
+
+    print("لطفا توجه کنید !\nplease pay attention!")
+    # تکرارشد
+Degree = int(input("  درجه را تنظیم میکنید ؟\nPlease adjust the degree :"))
+if  Degree <= 50:
+    print(" درجه کمه بیشترش کن :\nThe degree decreased. Do more:")
+elif Degree  <=59:
+    print(" درجه رایه کم زیادکن :\nThe degree decreased. Do more :")
+elif Degree == 60:
+    print("بهترین تنظیم کارخانه :\nBest factory setting:")
+elif Degree  <= 69:
+    print(" درجه رایه کم کمش کن :\nIncrease the degree :")
+elif Degree  <= 79:
+    print(" درجه زیادشدکمش کن :\nIncrease the degree :")
+elif Degree <= 99:
+    print(" درجه خیلی زیاد شدکمش کن :\nIncrease the degree :")
+
+    print("لطفا توجه کنید !\nplease pay attention!")
+    # تکرارشد
+Degree = int(input("  درجه رادوباره تنظیم میکنید ؟\nPlease adjust the degree :"))
+if  Degree <= 50:
+    print(" درجه کمه بیشترش کن :\nThe degree decreased. Do more:")
+elif Degree  <=59:
+    print(" درجه رایه کم زیادکن :\nThe degree decreased. Do more :")
+elif Degree == 60:
+    print("بهترین تنظیم کارخانه :\nBest factory setting:")
+elif Degree  <= 69:
+    print(" درجه رایه کم کمش کن :\nIncrease the degree :")
+elif Degree  <= 79:
+    print(" درجه زیادشدکمش کن :\nIncrease the degree :")
+elif Degree <= 99:
+    print(" درجه خیلی زیاد شدکمش کن :\nIncrease the degree :")
+
+    print("لطفا توجه کنید !\nplease pay attention!")
+    # تکرارشد
+Degree = int(input("  درجه رادوباره تنظیم کنید ؟\nPlease adjust the degree :"))
+if  Degree <= 50:
+    print(" درجه کمه بیشترش کن :\nThe degree decreased. Do more:")
+elif Degree  <=59:
+    print(" درجه رایه کم زیادکن :\nThe degree decreased. Do more :")
+elif Degree == 60:
+    print("بهترین تنظیم کارخانه :\nBest factory setting:")
+elif Degree  <= 69:
+    print(" درجه رایه کم کمش کن :\nIncrease the degree :")
+elif Degree  <= 79:
+    print(" درجه زیادشدکمش کن :\nIncrease the degree :")
+elif Degree <= 99:
+    print(" درجه خیلی زیاد شدکمش کن :\nIncrease the degree :")
+
+    print("لطفا توجه کنید !\nplease pay attention!")
+    # تکرارشد
+Degree = int(input("  درجه رادوباره تنظیم میکنید ؟\nPlease adjust the degree :"))
+if  Degree <= 50:
+    print(" درجه کمه بیشترش کن :\nThe degree decreased. Do more:")
+elif Degree  <=59:
+    print(" درجه رایه کم زیادکن :\nThe degree decreased. Do more :")
+elif Degree == 60:
+    print("بهترین تنظیم کارخانه :\nBest factory setting:")
+elif Degree  <= 69:
+    print(" درجه رایه کم کمش کن :\nIncrease the degree :")
+elif Degree  <= 79:
+    print(" درجه زیادشدکمش کن :\nIncrease the degree :")
+elif Degree <= 99:
+    print(" درجه خیلی زیاد شدکمش کن :\nIncrease the degree :")
+
+    print("لطفا توجه کنید !\nplease pay attention!")
+    # تکرارشد
+Degree = int(input("  درجه رادوباره تنظیم میکنید ؟\nPlease adjust the degree :"))
+if  Degree <= 50:
+    print(" درجه کمه بیشترش کن :\nThe degree decreased. Do more:")
+elif Degree  <=59:
+    print(" درجه رایه کم زیادکن :\nThe degree decreased. Do more :")
+elif Degree == 60:
+    print("بهترین تنظیم کارخانه :\nBest factory setting:")
+elif Degree  <= 69:
+    print(" درجه رایه کم کمش کن :\nIncrease the degree :")
+elif Degree  <= 79:
+    print(" درجه زیادشدکمش کن :\nIncrease the degree :")
+elif Degree <= 99:
+    print(" درجه خیلی زیاد شدکمش کن :\nIncrease the degree :")
+
+    print("لطفا توجه کنید !\nplease pay attention!")
+    # تکرارشد
+
+else:
+    print(" خوب دیگه دستش نزن :\nWell, do not touch it anymore :")
+
