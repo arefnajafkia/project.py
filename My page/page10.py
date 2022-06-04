e6b6ed6200ea6851d6057f84de4f04b48aa37d85
aref najafkia <arefnajafkia294@gmail.com>
2022-06-05 00:44:22 +0430

    added new my page10.py and arefnajafkia website.html

---
+++ My page/page10.py
@@ -0,0 +1,28 @@
+#ماژول پایتون برای ردیابی و تشخیص
+# FIRST ֲ®  اشیاء بینایی کامپیوتری عمدتاً برای برنامه مسابقه رباتیک
+#python -m pip install ovl[cv]
+#For the full installation of all features use: python -m pip install ovl[full]
+#For the frc related features use the frc option: python -m pip install ovl[frc]
+print ()
+
+import ovl
+
+target_filters = [ovl.percent_area_filter(min_area=0.005),
+                  ovl.circle_filter(min_area_ratio=0.7),
+                  ovl.area_sort()]
+
+threshold = ovl.Color([20, 100, 100], [55, 255, 255])
+
+yellow_circle = ovl.Vision(threshold=threshold,
+                           target_filters=target_filters,
+                           camera=0,  # open the first connected camera
+                           image_filters=[ovl.gaussian_blur()])
+
+while True:
+    image = yellow_circle.get_image()
+    targets, filtered_image = yellow_circle.detect(image)
+    directions = yellow_circle.get_directions(targets, filtered_image)
+
+    print(directions)  # prints out the (x, y) coordinates of the largest target
+
+# مبنع پروزه https://pypi.org/project/ovl/
