ab641b951295b5502b5345bd4b8bfaccc6a7f131
aref najafkia <arefnajafkia294@gmail.com>
2022-06-08 22:43:58 +0430

    added modinew my page13.py

---
+++ My page1/page13.py
@@ -0,0 +1,17 @@
+#python3
+#توليد رمزعبوريکبارمصرف باپايتون3
+#فقط کافيه شماتعدادکارکترهارابگوييدوبه همان تعداد به شما يک رمز
+#بااون تعداد کارکتري که گفته بوديد ميده بي نهايت ميشه انجام داد
+
+
+import random
+print(''' Welcome To Password Generator ... ''')
+pass_range = int(input('Please Enter The Password Range : \n لطفا محدوده رمز عبور را وارد کنید :'))
+alefba = ('abcdefglomnp123456789_*#$@!%ASDFGHJKLMNBVCXZQWERTYUIOP')
+if pass_range >= 8:
+    for i in range(pass_range):
+        i = random.choice(alefba)
+        print(i, end= '')
+        
+else:
+    print('You Can Not Choice >8 Number ! \n کمتراز8 کارکترنمي شود !')
