f029ea2baec4106b15d0928a50864bc0644382de
aref najafkia <arefnajafkia294@gmail.com>
2022-06-08 19:23:25 +0430

    added modnew mypage12.py باری با تاس

---
+++ My page1/page12.py
@@ -0,0 +1,26 @@
+#python3
+# بازي با تاس تاهرچقدر بخواهيد ميتوانيد بازي کنيد
+import random
+
+
+while True:
+    start = input ('Start ? (Y/N) \n شروع کن :').upper()
+    if start == 'Y':
+        a = random.randint(1,6)
+        print (a,':شماره شما شد ')
+        b = random.randint(1,6)
+        print (b,':شماره من شد ')
+        print()
+    if a>b:
+        print('You came 😯 شمابرديد')
+    if a<b:
+        print ('I won 😀 من بردم')
+    if a == b:
+        print ('We were equal 😊 مساوي شديم ')
+        print()
+        print(5*'=')
+    else:
+        print ('Program Closed ! \n اين مرحله تموم شد! ')
+        print()
+        print (5*'=')
+        pass
