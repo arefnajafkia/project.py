ac28404fb759fe2a94bf09d58eee7dfef3516ed7
aref najafkia <arefnajafkia294@gmail.com>
2022-06-10 20:11:43 +0430

    added new modi my page14.py and Data2.txt

---
+++ My page1/page14.py
@@ -0,0 +1,21 @@
+
+#python3
+#تاس انداختن چندتايي 
+#باچندتاتاس ميخواهي بازي کني 
+#هم ميشه دوتاسه بازي کردمناسب تخته نربازي وهم اينکه چندتايي وهم تکي
+
+import random
+
+
+while True:
+    playerDecision = int (input ("what you want to do ?\n چيکارميکنيدشما؟ اگه تاس ميريزي 1روبزن واگرميخواهي خارج بشي 2روبزن \n 1.roll the dice  2.exit \n"))
+    if playerDecision == 1:
+        totalplayers = int (input ("How many dice do you want to play with ? \n باچندتاتاس ميخواهي بازي کني؟ :"))
+        playerNumber = 1
+        for player in range(totalplayers):
+            result = random.randint(1,6)
+            print (f"Result for {playerNumber} is:" , result)
+            playerNumber+=1
+            print()
+    else:
+        break
